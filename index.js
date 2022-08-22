f61abeb48284069c201c1d3c205f4ddc4c352a4c
Mike Stolley <stolleywilliam@yahoo.com>
2022-08-22 17:45:12 -0400

    added input for manager, and added a function to add a team member, and redirect to their respection input

--- index.js
+++ index.js
@@ -9,3 +9,62 @@ const outputDirectory = path.resolve(dirname, "output");
 const outputPath = path.join(outputDirectory, "team.html");
 const teamMembers = [];
 
+
+function start() {
+    managerInput();
+}
+
+managerInput = () => {
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your team managers name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your team managers Id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your team managers email?"
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "What is your team managers office number?"
+        }
+    ])
+    .then((val) => {
+        const manager = new Manager (
+            val.name,
+            val.id,
+            val.email,
+            val.office
+        );
+        teamMembers.push(manager);
+        addTeamMember();
+    });
+}
+
+addTeamMember = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "type",
+            message: "Would you like to add an engineer or intern?",
+            choices: ["Engineer", "Intern", "None"],
+        }
+    ])
+    .then((val) => {
+        if (val.type === "Engineer") {
+            engineerQuestions();
+        } else if (val.type === "Intern") {
+                internQuestions();
+            } else {
+                createTeam();
+            }
+    });
+}

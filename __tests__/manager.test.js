905d22a6964c5a91786ef700d676b2184e6dc94e
Mike Stolley <stolleywilliam@yahoo.com>
2022-08-23 18:58:12 -0400

    adjusted test positions to get a pass result

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -1,6 +1,13 @@
 const Employee = require("../lib/employee")
 const Manager = require("../lib/manager")
 
+
+test("Set the office number", () => {
+    const testVar = 100;
+    const e = new Manager("abc", 1, "test@gmail.com", testVar);
+    expect(e.officeNumber).toBe(testVar)
+});
+
 test = () => {
     const testVar = "Manager";
     const e = new Manager("abc", 1, "test@gmail.com", 100);
@@ -13,8 +20,3 @@ test = () => {
     expect(e.getOfficeNumber()).toBe(testVar)
 };
 
-test("Set the office number", () => {
-    const testVar = 100;
-    const e = new Manager("abc", 1, "test@gmail.com", testVar);
-    expect(e.officeNumber).toBe(testVar)
-});

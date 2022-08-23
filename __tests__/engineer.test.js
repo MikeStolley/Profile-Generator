cba956648f7536e6f7b9a996a041d2c7c9a6bb16
Mike Stolley <stolleywilliam@yahoo.com>
2022-08-23 14:16:38 -0400

    ran initial test for engineer profile

---
+++ __tests__/engineer.test.js
@@ -0,0 +1,21 @@
+const Engineer = require("../lib/engineer")
+
+test("create github account", () => {
+    const testVar = "GitHub Username";
+    const e = new Engineer("abc", 1, "test@gmail.com", testVar);
+    expect(e.github).toBe(testVar)
+});
+
+test("getRole for engineer", () => {
+    const testVar = "Engineer";
+    const e = new Engineer("abc", 1, "test@gmail.com", "GitHub Username");
+    expect(e.getRole()).toBe(testVar)
+});
+
+test("get Github username", () => {
+    const testVar = "GitHub Username";
+    const e = new Engineer("abc", 1, "test@gmail.com", testVar);
+    expect(e.getGithub()).toBe(testVar)
+});
+
+

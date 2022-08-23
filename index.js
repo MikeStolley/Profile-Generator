f4db739d6b7f0e328bbe2aa6a482ab342470d16f
Mike Stolley <stolleywilliam@yahoo.com>
2022-08-23 02:32:23 -0400

    finished intern and engineer questions, added function to write and create file to correct folder

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs")
 const Engineer = require("./lib/engineer")
 const Intern = require("./lib/intern")
 const Manager = require("./lib/manager")
+const { start } = require("repl")
 
 const outputDirectory = path.resolve(dirname, "output");
 const outputPath = path.join(outputDirectory, "team.html");
@@ -68,3 +69,75 @@ addTeamMember = () => {
             }
     });
 }
+
+engineerQuestions = () => {
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter your engineers name",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter your engineers ID: ",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your engineers email address",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your engineers GitHub Username?",
+        },
+    ])
+    .then((val) => {
+        const engineer = new Engineer(val.name, val.id, val.email, val.github);
+        teamMembers.push(engineer);
+        addTeamMember();
+    });
+}
+
+internQuestions = () => {
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter your interns name",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter your interns ID: ",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your interns email",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What school does your intern attend?",
+        },
+    ])
+    .then((val) => {
+        const intern = new Intern(val.name, val.id, val.email, val.school);
+        teamMembers.push(intern);
+        addTeamMember();
+    });
+}
+
+// Utilize exsistsSync to return whether or not a path exsists, and if not, create one
+
+createDoc = () => {
+    if (!fs.existsSync(outputDirectory)) {
+        fs.mkdirSync(outputDirectory);
+    } else {
+        fs.writeFileSync(outputPath, render(teamMembers), "UTF-8");
+    }
+}
+
+start();
